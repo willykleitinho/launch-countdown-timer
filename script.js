24af3586b873eea2f5736a50652643adeaee19bf
Wellington <hanzo.hasashi.pmk@gmail.com>
2021-11-14 02:19:50 -0300

    Refactor

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-const targetDate = new Date('February 9, 2022');
-console.log(targetDate.toString());
-
-var daysLeft, hoursLeft, minutesLeft, secondsLeft;
-
 window.onload = () => {
+  const format = (value, factor) => String(Math.floor((value / factor))).padStart(2, '0');
 
-  daysLeft = document.getElementById('days-left');
-  hoursLeft = document.getElementById('hours-left');
-  minutesLeft = document.getElementById('minutes-left');
-  secondsLeft = document.getElementById('seconds-left');
+  const targetDate = new Date('February 9, 2022');
+
+  const daysLeft = document.getElementById('days-left');
+  const hoursLeft = document.getElementById('hours-left');
+  const minutesLeft = document.getElementById('minutes-left');
+  const secondsLeft = document.getElementById('seconds-left');
   
   setInterval(() => {
-    let temp = targetDate - Date.now();
+    let milliseconds = targetDate - Date.now();
 
     // a day has 86.400.000 milliseconds
-    daysLeft.innerText = String(Math.floor((temp / 86400000))).padStart(2, '0');
-    temp %= 86400000;
+    daysLeft.innerText = format(milliseconds, 86400000);
+    milliseconds %= 86400000;
 
     // an hour has 3.600.000 milliseconds
-    hoursLeft.innerText = String(Math.floor((temp / 3600000))).padStart(2, '0');
-    temp %= 3600000;
+    hoursLeft.innerText = format(milliseconds, 3600000);
+    milliseconds %= 3600000;
 
     // a minute has 60.000 milliseconds
-    minutesLeft.innerText = String(Math.floor((temp / 60000))).padStart(2, '0');
-    temp %= 60000;
+    minutesLeft.innerText = format(milliseconds, 60000);
+    milliseconds %= 60000;
 
     // a second has 1.000 milliseconds
-    secondsLeft.innerText = String(Math.floor((temp / 1000))).padStart(2, '0');
-  }, 999);
+    secondsLeft.innerText = format(milliseconds, 1000);
+  }, 990);
 }
